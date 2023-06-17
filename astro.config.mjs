9066a38503ad707b77b57e9e046ee325dd3cdfa0
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2023-06-17 12:04:24 -0700

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,15 +11,53 @@ export default defineConfig({
       },
       sidebar: [
         {
-          label: 'Guides',
+          label: 'Overview',
           items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
+            { label: 'About Me', link: '/me/' },
+            { label: 'What is this site?', link: '/site/' },
+            { label: 'Style Guide', link: '/style-guide/' },
           ],
         },
         {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
+          label: 'Open Source Projects',
+          items: [
+            { label: 'Accessible for All', link: '/oss/accessible-for-all/' },
+            { label: 'Astro', link: '/oss/astro/' },
+            { label: 'Global Chemistry', link: '/oss/global-chem/' },
+            { label: 'Material UI', link: '/oss/mui/' },
+          ],
+        },
+        {
+          label: 'Back-end Development',
+          items: [
+            { label: 'Budget Buddy', link: '/backend/budget-buddy/' },
+            { label: 'Task It', link: '/backend/task-it/' },
+          ],
+        },
+        {
+          label: 'Front-end Development',
+          items: [
+            { label: 'Documentation Site', link: '/frontend/docs/' },
+            { label: 'Happy Hour Guide', link: '/frontend/happy-hour/' },
+            { label: 'Portfolio Site', link: '/frontend/portfolio/' },
+            { label: 'Poke-Dex', link: '/frontend/poke-dex/' },
+            { label: 'Welcome to Space', link: '/frontend/space/' },
+          ],
+        },
+        {
+          label: 'Full Stack Development',
+          items: [
+            { label: 'Track My Funds', link: '/fullstack/tmf/' },
+          ],
+        },
+        {
+          label: 'Additional Information',
+          items: [
+            { label: 'My Socials', link: '/socials/' },
+            { label: 'My Resume', link: '/resume/' },
+            { label: 'Contact Me', link: '/contact/' },
+            { label: 'My Blog', link: '/blog/' },
+          ],
         },
       ],
     }),
