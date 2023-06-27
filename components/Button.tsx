c417bccac4742b6d5953886c55f5e37d6ebabac0
gerrardsarah <gerrardsarah@gmail.com>
2023-06-26 23:01:45 -0700

    buttons and card

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,19 +1,63 @@
 import { Button } from "@nextui-org/react";
 import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 
-export default function Buttons({ type, name }: { type: string, name?: string }) {
-
-    if (type === "GitHub") {
-        return (
-            <Button icon={<FaGithub />} color="success">
-                GitHub
-            </Button>
-        );
-    }
-
+export default function Buttons({
+  repo,
+  href,
+}: {
+    repo?: string;
+    href?: string;
+}) {
+  if (!href) {
     return (
-        <Button icon={<FaExternalLinkAlt />} color="success">
-            {name}
-        </Button>
+      <Button
+        icon={<FaGithub />}
+        onClick={() => {
+          window.open(href, "_blank");
+        }}
+        css={{
+          backgroundColor: "#000",
+          border: "1px solid white",
+        }}
+      >
+        GitHub
+      </Button>
     );
+  }
+
+  return (
+    <div style={{
+        display: "flex",
+        flexWrap: "wrap",
+        flexDirection: "row",
+    }}>
+        <Button
+        icon={<FaGithub />}
+        onClick={() => {
+          window.open(repo, "_blank");
+        }}
+        css={{
+          backgroundColor: "#000",
+          border: "1px solid white",
+          margin: "10px"
+        }}
+      >
+        GitHub
+      </Button>        
+        <Button
+      icon={<FaExternalLinkAlt />}
+      color="primary"
+      onClick={() => {
+        window.open(href, "_blank");
+      }}
+      css={{
+        border: "1px solid white",
+        margin: "10px"
+      }}
+    >
+      Website
+    </Button>
+    </div>
+    
+  );
 }
